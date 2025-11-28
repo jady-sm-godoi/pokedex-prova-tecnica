650ac2c56af53d904bd41a6520b67eba7a8fedb9
Jady Godoi <jady.godoi@oi.net.br>
2025-11-28 08:22:55 -0300

    refactor: extracao da logica de fetching em funcao dedicada

--- script.js
+++ script.js
@@ -7,24 +7,35 @@ const getPokemonData = (pokemons) => {
     }));
 }
 
-const handlePageLoad = async () => {
-
+const fetchPokemonsFromAPI = async () => {
     try {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0.');
-
+        
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        const { results: pokemonsList } = await response.json();
+        const {results: pokemonsList} = await response.json();
 
-        // Fetch detailed data for each Pokémon
         const pokemonsData = await Promise.all(
-            pokemonsList.map(pokemon => fetch(pokemon.url).then(response => response.json()))
+            pokemonsList.map(pokemon => fetch(pokemon.url).then(r => r.json()))
         );
 
-        const pokemonsWithDataAndImages = getPokemonData(pokemonsData);
+        return { pokemonsList, pokemonsData };
+    } catch (error) {
+        console.error("Error fetching pokemons from API:", error);
+        throw error;
+    }
+}
 
-        console.log("all", pokemonsWithDataAndImages);
+const handlePageLoad = async () => {
+
+    try {
+        const { pokemonsList, pokemonsData } = await fetchPokemonsFromAPI();
+
+        const pokemonsWithDataAndImage = getPokemonData(pokemonsData);
+
+        console.log("list pokemons:", pokemonsList);
+        console.log("list dados necessarios dos pokemons:", pokemonsWithDataAndImage);
 
 
     } catch (error) {
